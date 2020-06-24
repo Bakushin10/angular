f659fcca87444e328f1d0a2c16981d5d4cc3ffa0
shin nagai <shnnagai@gmail.com>
2020-06-24 23:37:03 +0900

    create form builder recursively

--- ng7/src/app/cache/cache.component.ts
+++ ng7/src/app/cache/cache.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, startWith, tap } from 'rxjs/operators';
+import { FormBuilderService } from '../form-builder.service'
+import { FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms'
+import { myFormModel } from '../models/myForm.model'
+import { OtherInfoModel } from '../models/ohterInfo.model'
 import { of } from 'rxjs'
+import { disableDebugTools } from '@angular/platform-browser';
 
 const CACHE_KEY = 'httpCache';
 
@@ -13,10 +18,14 @@ const CACHE_KEY = 'httpCache';
 
 export class CacheComponent implements OnInit {
   repos$;
-  
+  myForm : FormGroup;
+
   CACHE_KEY = 'httpCache';
-  constructor(private http: HttpClient) {
-  }
+  constructor(
+    private http: HttpClient,
+    private formBuilderService : FormBuilderService,
+    private formBuilder : FormBuilder
+    ) {}
 
   ngOnInit(): void {
     const url = 'https://api.github.com/search/repositories?q=angular';
@@ -34,6 +43,19 @@ export class CacheComponent implements OnInit {
     this.repos$ = this.repos$.pipe(
       startWith(JSON.parse(localStorage[CACHE_KEY] || ''))
     )
+    let otherInfo = new OtherInfoModel(100, "300");
+    let myFormInput = new myFormModel(
+      "shin",
+      "nagai",
+      "5/10",
+      otherInfo
+    );
+    console.log(myFormInput)
+    console.log(myFormInput.otherInfo)
+    this.myForm = this.formBuilderService.buildForm(myFormInput);
+    //this.myForm = this.formBuilder.group({})
+    debugger
+    console.log(this.myForm)
   }
 
 }
